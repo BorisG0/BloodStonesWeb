aad6e52fd838399e7175930075e601795500bf6a
boris <boris@gratchev.com>
2021-10-16 21:17:47 +0200

    handcard drawing

--- game.js
+++ game.js
@@ -6,11 +6,25 @@ function startGame(){
     canvas.width = window.innerWidth;
 
 
+    var imgLogo = document.getElementById("KidneyStoneLogoImage");
+    var logoSizeX = 1536/2;
+    var logoSizeY = 724/2;
+    ctx.drawImage(imgLogo, (canvas.width / 2) - (logoSizeX / 2), 10, logoSizeX, logoSizeY);
+    
+
+    drawActiveHand();
+    
+}
+
+
+function drawActiveHand(){
+
+    var canvas = document.getElementById("myCanvas");
+    var ctx = canvas.getContext("2d");
+
     var imgCreatureGoblin = document.getElementById("CreatureGoblinImage");
+    var handCardSizeX = 130;
+    var handCardSizeY = handCardSizeX * Math.sqrt(2);
 
-
-    var handImageSizeX = 160;
-    var handImageSizeY = handImageSizeX * Math.sqrt(2);
-
-    ctx.drawImage(imgCreatureGoblin, 10, 10, handImageSizeX, handImageSizeY);
+    ctx.drawImage(imgCreatureGoblin, 0, canvas.height-handCardSizeY,handCardSizeX, handCardSizeY)
 }
