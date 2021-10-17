e3580aebe40b9ee147b189a436c3dea75aa0771c
boris <boris@gratchev.com>
2021-10-17 20:18:12 +0200

    Function for drawing Card

--- game.js
+++ game.js
@@ -11,6 +11,12 @@ function startGame(){
     var logoSizeY = 724/2;
     ctx.drawImage(imgLogo, (canvas.width / 2) - (logoSizeX / 2), 10, logoSizeX, logoSizeY);
     
+    ctx.font = '50px serif';
+
+    const goblinCard = new Card("goblin", document.getElementById("CreatureGoblinImage"));
+
+    drawCard(goblinCard,10,10,130);
+    
 
     drawActiveHand();
     
@@ -26,5 +32,31 @@ function drawActiveHand(){
     var handCardSizeX = 130;
     var handCardSizeY = handCardSizeX * Math.sqrt(2);
 
-    ctx.drawImage(imgCreatureGoblin, 0, canvas.height-handCardSizeY,handCardSizeX, handCardSizeY)
+
+    ctx.drawImage(goblinCard.image, 0, canvas.height-handCardSizeY,handCardSizeX, handCardSizeY);
+    ctx.fillText("goblinCard.name", 0 ,0 ,100 ,100 );
+
+    drawCard(goblinCard, 0, canvas.height-handCardSizeY,handCardSizeX);
+}
+
+function drawCard(card, x, y, size){
+    var canvas = document.getElementById("myCanvas");
+    var ctx = canvas.getContext("2d");
+
+    ctx.drawImage(card.image, x, y, size, size * Math.sqrt(2));
+}
+
+
+class Card{
+    constructor(name, image){
+        this.name = name;
+        this.image = image;
+
+    }
+}
+
+class Player{
+    constructor(name){
+        this.name = name;
+    }
 }
